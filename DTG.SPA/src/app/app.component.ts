2b2a7dc4cc7bcd71e823ea20ca0c2a541c8a2959
Kenny Whyte <hulkbaby2@hotmail.com>
2019-07-27 16:26:07 -0400

    updated angular.json

--- DTG.SPA/src/app/app.component.ts
+++ DTG.SPA/src/app/app.component.ts
@@ -1,35 +1,27 @@
-import { JwtHelperService } from '@auth0/angular-jwt';
-import { AuthService } from './_services/auth.service';
 import { Component, OnInit } from '@angular/core';
-import { User } from './_models/User';
+import { AuthService } from './_services/auth.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
+  jwtHelper = new JwtHelperService();
 
-
-  constructor(private authService:AuthService, private jwtHelperService: JwtHelperService){}
-
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
-   const token = localStorage.getItem('token');
-   const user: User = JSON.parse(localStorage.getItem('user'));
-   if(token){
-     this.authService.decodedToken = this.jwtHelperService.decodeToken(token);
-   }
-
-   if (user){
-    this.authService.currentUser = user;
-    if(this.authService.currentUser.photoUrl !==null){
-      this.authService.changeMemeberPhoto(user.photoUrl);
+    const token = localStorage.getItem('token');
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    if (token) {
+      this.authService.decodedToken = this.jwtHelper.decodeToken(token);
     }
-    else{
-
-      this.authService.changeMemeberPhoto('../assets/user.png')
+    if (user) {
+      this.authService.currentUser = user;
+      this.authService.changeMemberPhoto(user.photoUrl);
     }
-   }
   }
 }
