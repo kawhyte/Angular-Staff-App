c8cd163936b362b92043b745f70a280f8af629bd
Kenny Whyte <kawhyte@gmail.com>
2019-07-30 01:41:55 -0400

    Photo links

--- DTG.SPA/src/app/_services/photo.service.ts
+++ DTG.SPA/src/app/_services/photo.service.ts
@@ -1,20 +1,22 @@
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class PhotoService {
+baseUrl = environment.apiUrl;
 
   constructor(private http: Http) { }
 
   upload(vehicleId, photo) {
     var formData = new FormData();
     formData.append('file', photo);
-    return this.http.post('http://localhost:5000/api/vehicles/' + vehicleId + "/photos", formData)
+    return this.http.post(this.baseUrl + '/vehicles/' + vehicleId + "/photos", formData)
       .map(res => res.json());
   }
 
   getPhotos(vehicleId) {
-    return this.http.get('http://localhost:5000/api/vehicles/' + vehicleId + "/photos")
+    return this.http.get( this.baseUrl+ '/vehicles/' + vehicleId + "/photos")
       .map(res => res.json());
   }
 
